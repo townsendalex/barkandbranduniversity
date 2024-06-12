edbaa8b58d78d4bb818828af8432117c6653da83
atownsend11 <74386903+atownsend11@users.noreply.github.com>
2024-06-12 18:57:46 -0400

    keys removed

--- app/public/wp-content/themes/bark-brand-university-theme/functions.php
+++ app/public/wp-content/themes/bark-brand-university-theme/functions.php
@@ -47,7 +47,7 @@ function pageBanner($args = NULL) {
 }
 
 function university_files() {
-    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyBnrbh4uHSb__JCWMy_FmiM6EdgVwaoCPg', NULL, '1.0', true);
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=GoogleApiKey', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -102,7 +102,7 @@ function university_adjust_queries($query) {
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api) {
-  $api['key'] = 'AIzaSyBnrbh4uHSb__JCWMy_FmiM6EdgVwaoCPg';
+  $api['key'] = 'GoogleApiKey';
   return $api;
 }
 
